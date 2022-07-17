54d05db38034e199131f04b6b76de22bfd50041e
thành nguyễn <thanhnguyen6702@gmail.com>
2022-07-17 20:03:27 +0700

    Update

--- src/test/java/Get_list_auctions/Get_list_auctions.java
+++ src/test/java/Get_list_auctions/Get_list_auctions.java
@@ -9,34 +9,20 @@ import static io.restassured.RestAssured.given;
 
 public class Get_list_auctions {
     @ParameterizedTest
-    @CsvFileSource(resources = "/auctions_by_status.csv",numLinesToSkip = 2)
-    public void Get_list_auctions(String status,String index,String count){
+    @CsvFileSource(resources = "/list_auctions.csv",numLinesToSkip = 2)
+    public void Get_list_auctions(String status,String index,String count,String type,String user_id,String category_id){
         BaseClass.init();
         Input input = new Input();
-        input.setIndex(index);
-        input.setCount(count);
+        input.index=index;
+        input.count=count;
         Response response = given().contentType(ContentType.JSON).when().body(input).get("/auctions/"+status);
         response.prettyPrint();
     }
 }
 class Input{
-    String index;
-    String count;
-    public String getIndex() {
-        return index;
-    }
-
-    public void setIndex(String index) {
-        this.index = index;
-    }
-
-    public String getCount() {
-        return count;
-    }
-
-    public void setCount(String count) {
-        this.count = count;
-    }
-
-
+    public String index;
+    public String count;
+    public String type;
+    public String user_id;
+    public String category_id;
 }
